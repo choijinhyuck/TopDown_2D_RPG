55137e566e856eec02c6d762ab6ecada1dd4a079
choijinhyuck <choijinhyuck@naver.com>
2023-11-29 10:25:39 +0900

    Object id에 따른 여러 문장 띄우기 기능 구현. Portrait 추가.

--- Assets/TopDown 2D RPG BE3/Scripts/GameManager.cs
+++ Assets/TopDown 2D RPG BE3/Scripts/GameManager.cs
@@ -1,27 +1,49 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public GameObject talkPanel;
+    public Image portraitImg;
     public Text talkText;
     public GameObject scanObject;
     public bool isAction;
+    public int talkIndex;
 
     public void Action(GameObject scanObj)
     {
-        if (isAction)
+
+        scanObject = scanObj;
+        ObjData objData = scanObj.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+        talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if (talkData == null)
         {
             isAction = false;
+            talkIndex = 0;
+            return;
+        }
+        if (isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1);
         }
         else
         {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은 " + scanObject.name + "입니다.";
+            talkText.text = talkData;
+            portraitImg.color = new Color(1, 1, 1, 0);
         }
-        talkPanel.SetActive(isAction);
+
+        isAction = true;
+        talkIndex++;
     }
 }
